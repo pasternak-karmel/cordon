86167e338add7ff42c7553d16361ee12854b240e
BrunelDev <ahokpossibrunel@gmail.com>
2025-02-11 18:18:58 +0100

    (refactor): some stuffs

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -9,38 +9,40 @@ import {
   SubscriptionsRecommandations,
 } from "@/app/_components/cards";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton";
 import { useNextPaymentStore } from "@/store/useNextPaymentStore";
 import { trpc } from "@/trpc/client";
+import { EmptyPage } from "./components/emptyPage";
 import EmptySubscription from "./components/emptySubscription";
-import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Dashboard() {
   const { subscription } = useNextPaymentStore();
   const { data: sub, isLoading } = trpc.getUserSubscriptions.useQuery();
+  const { data: hasLinkedAccount } = trpc.hasLinkedAccount.useQuery();
   if (isLoading) {
     return (
       <div className="px-6 pt-8">
-      {/* Spendings Section Header */}
-      <Skeleton className="h-8 w-32 my-3" />
+        {/* Spendings Section Header */}
+        <Skeleton className="h-8 w-32 my-3" />
 
-      {/* Charts Section */}
-      <div className="w-full flex flex-row gap-4">
-        {/* Bar Chart Skeleton */}
-        <div className="w-[450px]">
-          <Skeleton className="h-[300px] w-full rounded-lg" />
-        </div>
-        
-        {/* Pie Chart and Credit Card Section */}
-        <div className="w-[300px] flex flex-col space-y-2">
-          <Skeleton className="h-[150px] w-full rounded-lg" />
-          <Skeleton className="h-[150px] w-full rounded-lg" />
-        </div>
-      </div>
+        {/* Charts Section */}
+        <div className="w-full flex flex-row gap-4">
+          {/* Bar Chart Skeleton */}
+          <div className="w-[450px]">
+            <Skeleton className="h-[300px] w-full rounded-lg" />
+          </div>
 
-      {/* Next Payments Section */}
-      <div className="mt-8">
-        <Skeleton className="h-8 w-40 mb-4" />
-        
+          {/* Pie Chart and Credit Card Section */}
+          <div className="w-[300px] flex flex-col space-y-2">
+            <Skeleton className="h-[150px] w-full rounded-lg" />
+            <Skeleton className="h-[150px] w-full rounded-lg" />
+          </div>
+        </div>
+
+        {/* Next Payments Section */}
+        <div className="mt-8">
+          <Skeleton className="h-8 w-40 mb-4" />
+
           <div className="flex flex-row flex-wrap gap-4">
             {Array.from({ length: 3 }).map((_, index) => (
               <div key={index} className="md:basis-1/2 lg:basis-[25%]">
@@ -50,30 +52,34 @@ export default function Dashboard() {
               </div>
             ))}
           </div>
-      </div>
+        </div>
 
-      {/* Bottom Section */}
-      <div className="flex flex-row gap-4 flex-wrap mt-4 h-[200px]">
-        {/* Recommendations Skeleton */}
-        <Skeleton className="h-full w-[350px] rounded-lg" />
+        {/* Bottom Section */}
+        <div className="flex flex-row gap-4 flex-wrap mt-4 h-[200px]">
+          {/* Recommendations Skeleton */}
+          <Skeleton className="h-full w-[350px] rounded-lg" />
 
-        {/* Recent Subscriptions Skeleton */}
-        <div className="h-full flex flex-col gap-3 p-3 w-[350px]">
-          <Skeleton className="h-6 w-48" />
-          <ScrollArea className="h-[150px]">
-            <div className="flex flex-col gap-3">
-              {Array.from({ length: 4 }).map((_, index) => (
-                <Skeleton key={index} className="h-16 w-full rounded-md" />
-              ))}
-            </div>
-          </ScrollArea>
+          {/* Recent Subscriptions Skeleton */}
+          <div className="h-full flex flex-col gap-3 p-3 w-[350px]">
+            <Skeleton className="h-6 w-48" />
+            <ScrollArea className="h-[150px]">
+              <div className="flex flex-col gap-3">
+                {Array.from({ length: 4 }).map((_, index) => (
+                  <Skeleton key={index} className="h-16 w-full rounded-md" />
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
         </div>
       </div>
-    </div>
-    )
-  }
-  else if (sub?.subscription.length === 0) return <EmptySubscription />
-    
+    );
+  } else if (sub?.subscription.length === 0) return <EmptySubscription />;
+  else if (!hasLinkedAccount)
+    return (
+      <div className="mt-5 h-[50vh]">
+        <EmptyPage />
+      </div>
+    );
   else
     return (
       <div className="px-4 sm:px-6 pt-8 h-fit">
@@ -100,27 +106,23 @@ export default function Dashboard() {
         {/* Next Payments Section */}
         <div className="mt-8">
           <h2 className="my-3 text-gray-800 text-xl">Next Payments</h2>
-            <div className="flex flex-col sm:flex-row flex-wrap gap-4">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <div
-                  key={index}
-                  className="w-full sm:w-[350px]"
-                >
-                  <div className="py-2">
-                    <SubscriptionCard
-                      title="Netflix"
-                      subscriptionCategory="Family Plan"
-                      startingDate={"29 Aug 2022"}
-                      endingDate={"28 Aug 2023"}
-                      remainingDays={12}
-                      subscriptionPrice={145}
-                      imageUrl="/netflix.jpg"
-                    />
-                  </div>
+          <div className="flex flex-col sm:flex-row flex-wrap gap-4">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <div key={index} className="w-full sm:w-[350px]">
+                <div className="py-2">
+                  <SubscriptionCard
+                    title="Netflix"
+                    subscriptionCategory="Family Plan"
+                    startingDate={"29 Aug 2022"}
+                    endingDate={"28 Aug 2023"}
+                    remainingDays={12}
+                    subscriptionPrice={145}
+                    imageUrl="/netflix.jpg"
+                  />
                 </div>
-              ))}
-            </div>
-            
+              </div>
+            ))}
+          </div>
         </div>
 
         {/* Bottom Section */}
