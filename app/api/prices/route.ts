fc639e807e4b3af2d17a6d78e32875482a784d97
BrunelDev <ahokpossibrunel@gmail.com>
2025-02-17 13:42:47 +0100

    (refactor): a bit of redis

--- app/api/prices/route.ts
+++ app/api/prices/route.ts
@@ -1,12 +1,18 @@
+import RedisCacheService from "@/utils/redis";
 import { stripe } from "@/utils/stripe/config";
 import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
+    const cachedPrices = await RedisCacheService.getCachedData("prices");
+    if (cachedPrices) {
+      return new NextResponse(JSON.stringify(cachedPrices), { status: 200 });
+    }
     const prices = await stripe.prices.list({
       active: true,
       expand: ["data.product"],
     });
+    await RedisCacheService.setCachedData("prices", prices.data); // Cache for 1 minute (60 seconds)
 
     return new NextResponse(JSON.stringify(prices.data), { status: 200 });
   } catch (error) {
